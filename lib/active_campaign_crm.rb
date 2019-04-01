20cdc16ffa8ec666dc45d59b82678e2d12acd72d
Nuno Correia <nunolopesrb@gmail.com>
2019-04-01 12:57:48 +0100

    adds basic gem skeleton created by bundler

---
+++ lib/active_campaign_crm.rb
@@ -0,0 +1,5 @@
+require "active_campaign_crm/version"
+
+module ActiveCampaignCrm
+  # Your code goes here...
+end
---
+++ spec/active_campaign_crm_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe ActiveCampaignCrm do
+  it "has a version number" do
+    expect(ActiveCampaignCrm::VERSION).not_to be nil
+  end
+end

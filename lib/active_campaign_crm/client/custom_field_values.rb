7a6fa712fdc11380255180d6d1e61e9c08c6f44f
Nuno Correia <nunolopesrb@gmail.com>
2019-04-03 00:48:31 +0100

    add custom field values interface

---
+++ lib/active_campaign_crm/client/custom_field_values.rb
@@ -0,0 +1,31 @@
+module ActiveCampaignCrm
+  class Client
+    module CustomFieldValues
+      def custom_field_values(params = {})
+        response = @connection.get('fieldValues', params)
+        response['fieldValues']
+      end
+
+      def custom_field_value(id)
+        response = @connection.get("fieldValues/#{id}")
+        response['fieldValue']
+      end
+
+      def create_custom_field_value(properties)
+        @connection.post('fieldValues', custom_field_value_body(properties))
+      end
+
+      def update_custom_field_value(id, properties)
+        @connection.put("fieldValues/#{id}", custom_field_value_body(properties))
+      end
+
+      def delete_custom_field_value(id)
+        @connection.delete("fieldValues/#{id}")
+      end
+
+      def custom_field_value_body(properties)
+        { 'fieldValue': properties }.to_json
+      end
+    end
+  end
+end

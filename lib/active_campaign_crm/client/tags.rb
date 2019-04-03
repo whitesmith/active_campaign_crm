51debef941b9aa0ac31789096abcf4c76f8d4532
Nuno Correia <nunolopesrb@gmail.com>
2019-04-03 12:00:19 +0100

    interface to link tags with contacts

--- lib/active_campaign_crm/client/tags.rb
+++ lib/active_campaign_crm/client/tags.rb
@@ -15,12 +15,25 @@ module ActiveCampaignCrm
         @connection.post('tags', tag_body(fields))
       end
 
+      def add_tag_to_contact(contact, tag)
+        @connection.post('contactTags', contact_link_tag_body(contact, tag))
+      end
+
       def update_tag(id, fields)
         @connection.put("tags/#{id}", tag_body(fields))
       end
 
       def delete_tag(id)
         @connection.delete("tags/#{id}")
+      end
+
+      def contact_link_tag_body(contact, tag)
+        {
+          'contactTag': {
+            'contact': contact,
+            'tag': tag
+          }
+        }.to_json
       end
 
       def tag_body(fields)
